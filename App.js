ab6d4ff46bdbd0022c368f7711e4411a5cb7ef59
gderuwez <gderuwez@gmail.com>
2018-09-25 16:29:31 +0200

    test

--- App.js
+++ App.js
@@ -1,103 +1,50 @@
+// Fixes isomorphic-fetch
+//GLOBAL.self = GLOBAL;
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
-import NewUser from './components/NewUser';
-import app from './components/firebaseAuth';
-import base from './components/firestore';
-import 'firebase/auth';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+// import {Button, Avatar, Header, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import { createStackNavigator } from 'react-navigation';
+// import RootStack from './components/router';
+import LoginDGR from './components/Login';
+// import ProjectDescription from './components/ProjectDescription';
+// import ProjectsList from './components/ProjectsList';
+// import HomeScreen from './components/HomeScreen';
+// import DetailsScreen from './components/DetailsScreen';
+// import SignUp from './components/SignUp';
 
 export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: 'enter your email',
-      password: 'enter a password',
-      error: ''
-    };
-  }
-
-
-  emailinputChange(input) {
-    this.setState({email: input});
-  }
-
-  passwordinputChange(input) {
-    this.setState({password: input});
-  }
-
-  submitNewUser() {
-    app.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
-  }
-
-  testfunction() {
-    base.addToCollection('users', {me: "rtuy"})
-    .then(() => {
-      //document is added to the collection
-    }).catch(err => {
-    //handle error
-    });
-    this.submitNewUser();
-  }
-
-  submitExistingUser() {
-    app.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
-  }
-
-  componentDidMount() {
-    app.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        console.log("yup" + user);
-        // ...
-  } else {
-    // User is signed out.
-    console.log("nop");
-    // ...
-  }
-});
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <NewUser text={this.state.email} function={(input)=>this.emailinputChange(input)}/>
-        <NewUser text={this.state.password} function={(input)=>this.passwordinputChange(input)}/>
-        <Button
-          onPress={()=>this.submitExistingUser()}
-          title="Sign in"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Button
-          onPress={()=>this.testfunction()}
-          title="signup"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-    );
+      <RootStack/>
+    )
   }
 }
+const RootStack = createStackNavigator(
+    {
+      // Home: HomeScreen,
+      // Details: DetailsScreen,
+      // ProjectDescription: ProjectDescription,
+      // ProjectsList: ProjectsList,
+      Login: LoginDGR,
+      // Signup: SignUp
+    },
+    {
+      initialRouteName: 'Login',
+    }
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    backgroundColor: 'rgb(240,240,240)',
+    //alignItems: 'center',
   },
+  header: {
+    width: "100%",
+  },
+
+  text :{
+    color: "white",
+  }
 });
