76b71ec19ec35ebe80f7afd7951151e7095fad83
Thibault Gribaumont <Thibault@Thibaults-MacBook-Pro.local>
2018-09-26 13:42:10 +0200

    Router

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 // import {Button, Avatar, Header, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 import { createStackNavigator } from 'react-navigation';
-// import RootStack from './components/router';
 import HeaderDGR from './components/Header';
 import LoginDGR from './components/Login';
 import ProjectDescription from './components/ProjectDescription';
@@ -12,51 +11,16 @@ import ProjectsList from './components/ProjectsList';
 import HomeScreen from './components/HomeScreen';
 import DetailsScreen from './components/DetailsScreen';
 import SignUp from './components/SignUp'
-// import SideBar from './components/SideBar'
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state={test:false};
   }
-  // componentWillMount() {
-  //     this.setState({test: true});
-  // }
-  // async componentWillMount() {
-  //   await Expo.Font.loadAsync({
-  //   'Roboto': require('native-base/Fonts/Roboto.ttf'),
-  //   'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-  //   'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
-  //  });
-  // }
-  
   
   render() {
     return (
-      // <ScrollView style={styles.container}>
-      //   {/* <View style={styles.header}>
-      //   <HeaderDGR/>
-      //   </View>
-      //   <LoginDGR/>
-      //   <ProjectsList></ProjectsList> */}
-      //   {/* <Text>Test</Text> */}
         <RootStack/>
-      //   {/* <HomeScreen/>
-      //   <DetailsScreen/> */}
-      // </ScrollView>
-      // <ScrollView style={styles.container}>
-      // <RootStack/>
-      // </ScrollView>
-      // );
-
-      // <ScrollView style={styles.container}>
-      //   <View style={styles.header}>
-      //   <HeaderDGR/>
-      //   </View>
-      //   <LoginDGR/>
-      //   {/* <ProjectsList></ProjectsList> */}
-      //   <SignUp/>
-      // </ScrollView>
     );
 
     }
