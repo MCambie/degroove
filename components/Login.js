8e9c36f7829c1c5d6f43910b4e6daad970e6fcfd
Geraldine <winand.geraldine@hotmail.com>
2018-09-26 10:35:01 +0200

    login + sign up update

--- components/Login.js
+++ components/Login.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import {View,Text, StyleSheet} from 'react-native';
-import {Button, Avatar, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import {View,Text, StyleSheet, Image} from 'react-native';
+import {Button} from 'react-native-elements';
+import {Input, Label, Item} from 'native-base';
 
 class LoginDGR extends React.Component{
 	render(){
 		return(
 			 <View style={styles.login}>
-       <Avatar
-           medium
-           source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
-           onPress={() => console.log("Works!")}
-           activeOpacity={0.7}
-       />
-       <FormLabel style={{fontSize: '50'}} >Login</FormLabel>
-       <FormInput />
-       {/* if error display :*/}
-       <FormValidationMessage>{'This field is required'}</FormValidationMessage>
-       <FormLabel>Password </FormLabel>
-       <FormInput />
-       {/* if error display :*/}
-       <FormValidationMessage>{'This field is required'}</FormValidationMessage>
-       <View style={styles.button}>
-           <Button
-            backgroundColor='rgb(79, 188, 132)'
-            small
-            title='Sign in' />
-           <Button
-            backgroundColor='rgb(79, 188, 132)'
-            small
-            title='Sign up' />
-        </View>
+         <Image
+					style={{width: 150, height: 150, borderRadius : 75}}
+					source={require('../assets/logo.png')}
+				/>
+
+          <Item floatingLabel style = {styles.item}>
+            <Label>Email</Label>
+            <Input/>
+          </Item> 
+
+          <Item floatingLabel style = {styles.item}>
+              <Label>Password</Label>
+              <Input />
+          </Item> 
+  
+        <View style={styles.button}>
+            <Button style={styles.click}
+              backgroundColor='rgb(79, 188, 132)'
+              small
+              title='Sign in' />
+
+             <View style = {styles.lineStyle} />
+       
+             <Text style ={styles.signUp}>
+              You don't have an account ?
+             </Text>
+
+            <Button style = {styles.click}
+                  backgroundColor='rgb(79, 188, 132)'
+                  title='Create an account' />
+          </View>
+
       </View>
 			)
 	}
@@ -39,18 +48,30 @@ export default LoginDGR;
 const styles = StyleSheet.create({
   login: {
     flex: 1,
-    marginTop: 50,
+    // marginTop: 10,
+    padding : 30,
     backgroundColor: 'rgb(240,240,240)',
     alignItems: 'center'
   },
-  font: {
-    fontSize: 50
-  },
-  button:{
-    flexDirection: 'row',
 
-  },
-  green:{
+ item:{
+  marginTop : 10
+ },
 
+  click : {
+    margin : 30,
+    width : 200
+  },
+
+  lineStyle:{
+    borderWidth: 0.5,
+    borderColor:'black',
+    margin:10,
+},
+
+  signUp : {
+    textAlign : 'center', 
+    marginTop : 30
   }
+
 });
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View,Text,Image, StyleSheet} from 'react-native';
-import {Card, FormLabel, FormInput, FormValidationMessage, CheckBox, SearchBar, Button } from 'react-native-elements'
+import {View,Text,Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {Card, SearchBar, Button, CheckBox, FormLabel} from 'react-native-elements'
 import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
+import {Input, Label, Form, Item, ListItem, Body} from 'native-base';
 import ProjectDetails from './ProjectDetails';
 
 
@@ -20,7 +21,8 @@ class SignUp extends React.Component{
             checkBox8 : false,
             checkBox9 : false,
             checkBox10 : false, 
-            checkBox11 : false
+            checkBox11 : false,
+            collapsed : false
         }
     }
 
@@ -36,91 +38,103 @@ class SignUp extends React.Component{
                 </Text>
                 
                 <View style = {styles.form}>
-               
-                    <FormLabel>First Name</FormLabel>
-                    <FormInput/>
-                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>First Name</Label>
+                            <Input />
+                        </Item> 
 
-                    <FormLabel>Last Name</FormLabel>
-                    <FormInput/>
-                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+                        <Item floatingLabel>
+                            <Label>Last Name</Label>
+                            <Input />
+                        </Item> 
 
-                    <FormLabel>Email</FormLabel>
-                    <FormInput/>
-                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+                        <Item floatingLabel>
+                            <Label>Email</Label>
+                            <Input />
+                        </Item> 
 
-                    <FormLabel>Password</FormLabel>
-                    <FormInput/>
-                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+                        <Item floatingLabel>
+                            <Label>Password</Label>
+                            <Input />
+                        </Item> 
 
-                    <FormLabel>Region</FormLabel>
-                    <Collapse>
-                        <CollapseHeader>
-                            <View>
-                                <Text>Click here</Text>
-                            </View>
-                        </CollapseHeader>
-                        <CollapseBody>
-                        <CheckBox
-                            title = 'Antwerp'
-                            checked = {this.state.checkBox1}
-                            onPress={() => this.setState({checkBox1:!this.state.checkBox1})}
+                        <FormLabel>Region</FormLabel>
+                            <Collapse isCollapsed = {this.state.collapsed}>
+                                <CollapseHeader>
+                                    <View>
+                                        <Button
+                                            title= "Choose"
+                                            onPress = {() => this.setState({collapsed:!this.state.collapsed                                                  })}
+                                        /> 
+                                    </View>
+                                 </CollapseHeader>
+                                 <CollapseBody>
+                                    <CheckBox 
+                                        title = 'Antwerp'
+                                        checked = {this.state.checkBox1}
+                                        onPress={() => this.setState({checkBox1:!this.state.checkBox1})}
 
-                        />
-                        <CheckBox
-                            title = 'Brussels'
-                            checked = {this.state.checkBox11}
-                            onPress={() => this.setState({checkBox11:!this.state.checkBox11})}
+                                    />
+                                    <CheckBox
+                                        title = 'Brussels'
+                                        checked = {this.state.checkBox11}
+                                        onPress={() => this.setState({checkBox11:!this.state.checkBox11})}
 
-                        />
-                        <CheckBox
-                            title = 'East Flanders'
-                            checked = {this.state.checkBox2}
-                            onPress={() => this.setState({checkBox2:!this.state.checkBox2})}
-                        />
-                        <CheckBox
-                            title = 'Flemish Brabant'
-                            checked = {this.state.checkBox3}
-                            onPress={() => this.setState({checkBox3:!this.state.checkBox3})}
-                        />
-                        <CheckBox
-                            title = 'Limburg'
-                            checked = {this.state.checkBox4}
-                            onPress={() => this.setState({checkBox4:!this.state.checkBox4})}
-                        />
-                        <CheckBox
-                            title = 'West Flanders'
-                            checked = {this.state.checkBox5}
-                            onPress={() => this.setState({checkBox5:!this.state.checkBox5})}
-                        />
-                        <CheckBox
-                            title = 'Hainaut'
-                            checked = {this.state.checkBox6}
-                            onPress={() => this.setState({checkBox6:!this.state.checkBox6})}
-                        />
-                        <CheckBox
-                            title = 'Liège'
-                            checked = {this.state.checkBox7}
-                            onPress={() => this.setState({checkBox7:!this.state.checkBox7})}
-                        />
-                        <CheckBox
-                            title = 'Luxembourg'
-                            checked = {this.state.checkBox8}
-                            onPress={() => this.setState({checkBox8:!this.state.checkBox8})}
-                        />
-                        <CheckBox
-                            title = 'Namur'
-                            checked = {this.state.checkBox9}
-                            onPress={() => this.setState({checkBox9:!this.state.checkBox9})}
-                        />    
-                        <CheckBox
-                            title = 'Walloon Brabant'
-                            checked = {this.state.checkBox10}
-                            onPress={() => this.setState({checkBox10:!this.state.checkBox10})}
-                        />                
-                        </CollapseBody>
-                    </Collapse>
-                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+                                    />
+                                    <CheckBox
+                                        title = 'East Flanders'
+                                        checked = {this.state.checkBox2}
+                                        onPress={() => this.setState({checkBox2:!this.state.checkBox2})}
+                                    />
+                                    <CheckBox
+                                        title = 'Flemish Brabant'
+                                        checked = {this.state.checkBox3}
+                                        onPress={() => this.setState({checkBox3:!this.state.checkBox3})}
+                                    />
+                                    <CheckBox
+                                        title = 'Limburg'
+                                        checked = {this.state.checkBox4}
+                                        onPress={() => this.setState({checkBox4:!this.state.checkBox4})}
+                                    />
+                                    <CheckBox
+                                        title = 'West Flanders'
+                                        checked = {this.state.checkBox5}
+                                        onPress={() => this.setState({checkBox5:!this.state.checkBox5})}
+                                    />
+                                    <CheckBox
+                                        title = 'Hainaut'
+                                        checked = {this.state.checkBox6}
+                                        onPress={() => this.setState({checkBox6:!this.state.checkBox6})}
+                                    />
+                                    <CheckBox
+                                        title = 'Liège'
+                                        checked = {this.state.checkBox7}
+                                        onPress={() => this.setState({checkBox7:!this.state.checkBox7})}
+                                    />
+                                    <CheckBox
+                                        title = 'Luxembourg'
+                                        checked = {this.state.checkBox8}
+                                        onPress={() => this.setState({checkBox8:!this.state.checkBox8})}
+                                    />
+                                    <CheckBox
+                                        title = 'Namur'
+                                        checked = {this.state.checkBox9}
+                                        onPress={() => this.setState({checkBox9:!this.state.checkBox9})}
+                                    />    
+                                    <CheckBox
+                                        title = 'Walloon Brabant'
+                                        checked = {this.state.checkBox10}
+                                        onPress={() => this.setState({checkBox10:!this.state.checkBox10})}
+                                    />                
+                                </CollapseBody>
+                            </Collapse>
+                    </Form>
+                
+
+                    {/* 
+
+                    >
                     
                     <FormLabel>Skills</FormLabel>
                     <SearchBar
@@ -128,15 +142,15 @@ class SignUp extends React.Component{
                         platform="android"
                         placeholder='Search' 
                     />
-                    <FormValidationMessage>{'This field is required'}</FormValidationMessage> 
+                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>  */}
                 </View> 
 
                 <Button
-                 title='button'
-                 styleButton = {{
-                   width : 500
-                }}
-               
+                 title='Create an account'
+                 backgroundColor = 'rgb(79, 188, 132)'  
+                 style = {{
+                    width : 200,
+                 }}         
                 /> 
             </View>      
 		);
@@ -146,7 +160,8 @@ class SignUp extends React.Component{
 const styles = StyleSheet.create({
     container :{
         flex : 1, 
-       alignItems : "center"
+        marginBottom : 400
+    //    alignItems : "center"
     },
     title : {
        fontSize : 20
